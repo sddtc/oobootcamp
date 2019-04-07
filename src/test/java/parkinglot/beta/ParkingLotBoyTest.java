d4d36a4be721815de0a73d3c149f5e30408b353d
sddtc <changhbaga@gmail.com>
2019-04-07 19:13:55 +0800

    2-2 down.

--- src/test/java/parkinglot/beta/ParkingLotBoyTest.java
+++ src/test/java/parkinglot/beta/ParkingLotBoyTest.java
@@ -17,4 +17,17 @@ public class ParkingLotBoyTest {
         Receipt receipt = parkingLotBoy.park(car);
         Assert.assertSame(car, parkingLot.pickBy(receipt));
     }
+
+    @Test
+    public void should_parking_1_car_in_the_second_success_when_parkingboy_parking_1_car_given_2_parking_lots_with_free_space_2_1() {
+        ParkingLot parkingLot = new ParkingLot(2);
+        ParkingLot parkingLot2 = new ParkingLot(1);
+        ParkingLotBoy parkingLotBoy = new ParkingLotBoy(asList(parkingLot, parkingLot2));
+
+        Car car = new Car();
+        Receipt receipt = parkingLotBoy.park(car);
+        Assert.assertSame(car, parkingLot.pickBy(receipt));
+    }
+
+
 }
